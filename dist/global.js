99e386b4b6f379eab802ff0b0fc9f0ffb3a0fdd8
Nux <Nux@fake.wikipedia.org>
2022-11-26 22:15:22 +0000

    -

--- dist/global.js
+++ dist/global.js
@@ -1,3 +1,9 @@
+/* @sa [[User:Nux/global.css]] */
+/* @sa [[User:Nux/vector.js]] */
+/* @sa [[User:Nux/vector.css]] */
+/* @sa [[User:Nux/common.js]] */
+/* @sa [[User:Nux/common.css]] */
+
 // if: edit
 if ( mw.config.get('wgAction') === 'edit' || mw.config.get('wgAction') === 'submit' ) {
 	mw.loader.load( 'https://meta.wikimedia.org/wiki/User:Nux/editToolsCollapse.js?action=raw&ctype=text/javascript' );
