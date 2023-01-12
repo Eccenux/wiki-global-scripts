30a81b67155e176ffc550a44ffef30722f228cd2
Nux <Nux@fake.wikipedia.org>
2023-01-12 21:28:53 +0000

    -

--- dist/global.js
+++ dist/global.js
@@ -8,3 +8,6 @@
 if ( mw.config.get('wgAction') === 'edit' || mw.config.get('wgAction') === 'submit' ) {
 	mw.loader.load( 'https://meta.wikimedia.org/wiki/User:Nux/editToolsCollapse.js?action=raw&ctype=text/javascript' );
 }
+
+// [[View_it!_Tool]]
+mw.loader.load( '//meta.wikimedia.org/w/index.php?title=User:SuperHamster/view-it-full.js&action=raw&ctype=text/javascript' ); // Backlink: [[meta:User:SuperHamster/view-it-full.js]]
