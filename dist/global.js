5321d0521b10a3c58b59f8442c3f31b31c358e28
Nux <Nux@fake.wikipedia.org>
2023-03-15 01:15:27 +0000

    -

--- dist/global.js
+++ dist/global.js
@@ -22,6 +22,7 @@ var is_edit = mw.config.get("wgAction") == "edit" || mw.config.get("wgAction") =
 
 // sync notification over tabs
 mw.loader.load( 'https://meta.wikimedia.org/w/index.php?action=raw&ctype=text/javascript&smaxage=21600&maxage=86402&title=User:Nux/notificationsSync.js' );
+mw.hook('userjs.NuxNotificationSync.init').add(function(nns, version) {console.log('[notificationSync] init hook', {nns:nns, v:version})});
 
 // collapse edit tools section
 if (is_edit) {
