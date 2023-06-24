f1f7e1c0b30d4f33224949551e7ac2c0bc427882
eccenux <eccenux+git@gmail.com>
2023-06-24 17:42:29 +0200

    :is to less

--- build.js
+++ build.js
@@ -1,11 +1,13 @@
 import less from 'less';
 import fs from 'fs';
 
-function build_less(callback) {
+function build_less() {
+	let srcLess = 'src/_main.less';
+	let dstCss = 'dist/global.css';
 	return new Promise(function(resolve, reject) {
-		fs.readFile('src/_main.less',function(error,data){
+		fs.readFile(srcLess,function(error,data){
 			if (!data) {
-				console.error('Unable to read main Less.');
+				console.error('Unable to read Less.', srcLess);
 				console.error(error);
 				reject('main-less-fail');
 				return;
@@ -18,14 +20,14 @@ function build_less(callback) {
 					reject('parse-less-fail');
 					return;
 				}
-				fs.writeFile('dist/global.css', css, function(saveError){
+				fs.writeFile(dstCss, css.css, function(saveError){
 					if (saveError) {
-						console.error('Unable to save css.');
+						console.error('Unable to save css.', dstCss);
 						console.error(saveError);
 						reject('write-output-css-fail');
 						return;
 					}
-					resolve(callback());
+					resolve();
 				});
 			});
 		});
@@ -33,7 +35,7 @@ function build_less(callback) {
 }
 
 (async () => {
-	console.log('done', new Date());
+	console.log(new Date().toISOString(), 'start');
 	await build_less();
-	console.log('done');
+	console.log(new Date().toISOString(), 'done');
 })();
